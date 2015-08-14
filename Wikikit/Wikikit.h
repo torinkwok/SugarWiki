af9e5943b6ef57c5d24183c0189b65a16c043189
Tong Kuo <tong-kuo@outlook.com>
2015-08-14 15:40:22 +0800

    Initial commit

---
+++ Wikikit/Wikikit.h
@@ -0,0 +1,19 @@
+//
+//  Wikikit.h
+//  Wikikit
+//
+//  Created by Tong G. on 8/14/15.
+//  Copyright © 2015 TongKuo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Wikikit.
+FOUNDATION_EXPORT double WikikitVersionNumber;
+
+//! Project version string for Wikikit.
+FOUNDATION_EXPORT const unsigned char WikikitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wikikit/PublicHeader.h>
+
+
