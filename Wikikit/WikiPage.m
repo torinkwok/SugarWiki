694b66959ea828e87011a939b20efd8a129cfcbc
Tong Kuo <tong-kuo@outlook.com>
2015-08-15 01:56:41 +0800

    Introduced `WikiNamespace` enum

---
+++ Wikikit/WikiPage.m
@@ -0,0 +1,31 @@
+//
+//  WikiPage.m
+//  Wikikit
+//
+//  Created by Tong G. on 8/15/15.
+//  Copyright © 2015 TongKuo. All rights reserved.
+//
+
+#import "WikiPage.h"
+
+@implementation WikiPage
+
++ ( instancetype ) pageWithJSONDict: ( NSDictionary* )_JSONDict
+    {
+    return [ [ [ self class ] alloc ] initWithJSONDict: _JSONDict ];
+    }
+
+- ( instancetype ) initWithJSONDict: ( NSDictionary* )_JSONDict
+    {
+    if ( !_JSONDict )
+        return nil;
+
+    if ( self = [ super init ] )
+        {
+        
+        }
+
+    return self;
+    }
+
+@end
