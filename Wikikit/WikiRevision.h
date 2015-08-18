ace26ecb38011304fe58e27af5ef8d1030c138f7
Tong Kuo <tong-kuo@outlook.com>
2015-08-19 00:52:43 +0800

    Appended the ASCII art

--- Wikikit/WikiRevision.h
+++ Wikikit/WikiRevision.h
@@ -1,10 +1,26 @@
-//
-//  WikiRevision.h
-//  Wikikit
-//
-//  Created by Tong G. on 8/15/15.
-//  Copyright © 2015 TongKuo. All rights reserved.
-//
+/*=============================================================================┐
+|             _  _  _       _                                                  |  
+|            (_)(_)(_)     | |                            _                    |██
+|             _  _  _ _____| | ____ ___  ____  _____    _| |_ ___              |██
+|            | || || | ___ | |/ ___) _ \|    \| ___ |  (_   _) _ \             |██
+|            | || || | ____| ( (__| |_| | | | | ____|    | || |_| |            |██
+|             \_____/|_____)\_)____)___/|_|_|_|_____)     \__)___/             |██
+|                                                                              |██
+|                       _  _  _ _ _     _ _     _       _                      |██
+|                      (_)(_)(_|_) |   (_) |   (_)  _  | |                     |██
+|                       _  _  _ _| |  _ _| |  _ _ _| |_| |                     |██
+|                      | || || | | |_/ ) | |_/ ) (_   _)_|                     |██
+|                      | || || | |  _ (| |  _ (| | | |_ _                      |██
+|                       \_____/|_|_| \_)_|_| \_)_|  \__)_|                     |██
+|                                                                              |██
+|                                                                              |██
+|                         Copyright (c) 2015 Tong Kuo                          |██
+|                                                                              |██
+|                             ALL RIGHTS RESERVED.                             |██
+|                                                                              |██
+└==============================================================================┘██
+  ████████████████████████████████████████████████████████████████████████████████
+  ██████████████████████████████████████████████████████████████████████████████*/
 
 #import "WikiJSONObject.h"
 
@@ -55,3 +71,29 @@
 + ( instancetype ) revisionWithJSONDict: ( NSDictionary* )_JSONDict;
 
 @end
+
+/*===============================================================================┐
+|                                                                                |
+|                           The MIT License (MIT)                                |
+|                                                                                |
+|                        Copyright (c) 2015 Tong Kuo                             |
+|                                                                                |
+| Permission is hereby granted, free of charge, to any person obtaining a copy   |
+| of this software and associated documentation files (the "Software"), to deal  |
+| in the Software without restriction, including without limitation the rights   |
+| to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      |
+|   copies of the Software, and to permit persons to whom the Software is        |
+|         furnished to do so, subject to the following conditions:               |
+|                                                                                |
+| The above copyright notice and this permission notice shall be included in all |
+|              copies or substantial portions of the Software.                   |
+|                                                                                |
+| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     |
+| IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       |
+| FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    |
+|  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        |
+| LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  |
+| OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  |
+|                                 SOFTWARE.                                      |
+|                                                                                |
+└===============================================================================*/
